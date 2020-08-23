21490e1ed8b7dea59dc354fe5d6a90f4307ba494
Emmanuel López Guerrero <manelpz@icloud.com>
2020-08-22 22:03:01 -0500

    spread operator

--- starter/script.js
+++ starter/script.js
@@ -208,6 +208,39 @@ console.log(ages.findIndex(cur => cur >= 18));
 console.log(ages.find(cur => cur >= 18));
 
 
+//spread
+
+    function addFourAges(a,b,c, d){
+        return a+b+c+d;
+    }
+    var sum1 = addFourAges(12,13,15,34);
+    console.log(sum1);
+
+    //ES5 send from array to variable
+    var ages = [12, 13, 15, 34];
+    var sum2 = addFourAges.apply(null, ages);
+    console.log(sum2);
+
+    //ES6
+
+    const sum3 = addFourAges(... ages);
+    console.log(sum3);
+
+//example
+const familySmith = ['john', 'Jane', 'Mark'];
+const familyMiller = ['Mary','bob'];
+const bigFamily = [...familySmith, 'lilly',...familyMiller];
+
+//example
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
+
+
+
+
 
 
 

22ba1f46c889e329456fb4fe210a05d864a2e65a
Emmanuel López Guerrero <manelpz@icloud.com>
2020-08-26 22:31:13 -0500

    default parameter

--- starter/script.js
+++ starter/script.js
@@ -280,9 +280,34 @@ isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
 
 
 
+//Default parameters
+
+//ES5
+function SmithPerson(firstName, yearOfBirth, lastName, nationality){
+    lastName === undefined ? lastName = 'smith' : lastName = lastName;
+    nationality === undefined ? nationality = 'mexican' : nationality = nationality;
+
+    this.firstName = firstName;
+    this.yearOfBirth = yearOfBirth;
+    this.lastName = lastName;
+    this.nationality = nationality;
+}
+
+var john = new SmithPerson('john', 1990);
 
 
 
+//ES6
+
+function SmithPerson(firstName, yearOfBirth, lastName = 'smith', nationality = 'mexican'){
+    
+    this.firstName = firstName;
+    this.yearOfBirth = yearOfBirth;
+    this.lastName = lastName;
+    this.nationality = nationality;
+}
+
+var john = new SmithPerson('john', 1990);
 
 
 

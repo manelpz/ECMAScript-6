83ec428e1d7dc07d1b0668b60e45dcf2f87630d4
Emmanuel López Guerrero <manelpz@icloud.com>
2020-08-26 22:00:37 -0500

    rest parameters

--- starter/script.js
+++ starter/script.js
@@ -239,6 +239,27 @@ const all = [h, ...boxes];
 Array.from(all).forEach(cur => cur.style.color = 'purple');
 
 
+//Rest parameters
+
+//ES5
+function isFullAge5(){
+    var argsArr = Array.prototype.slice.call(arguments);
+    argsArr.forEach(function(cur){
+        console.log((2016 - cur) >= 18);
+    })
+}
+
+isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+//ES6
+function isFullAge6(...years){
+    years.forEach(cur => console.log((2016 - cur)>= 18));
+
+}
+
+isFullAge6(1990, 1999, 1965, 2016, 1987);
+
+
 
 
 
